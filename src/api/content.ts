b64ae25ca46602c6a8d05a0b847076915d04d417
Zahra memar <zahra.memar72@gmail.com>
2022-11-13 20:51:57 +0100

    Fix usage of random

--- src/api/content.ts
+++ src/api/content.ts
@@ -3,6 +3,6 @@ import data from './data.json';
 const wait = (ts: number) => new Promise(res => setTimeout(res, ts));
 
 export const getPage = async () => {
-	await wait(Math.random(2000));
+	await wait(Math.random()*2000);
 	return data;
 }
