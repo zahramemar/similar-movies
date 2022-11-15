c07fb69e5feba29f315e56fd79b3f391347aacb4
Zahra memar <zahra.memar72@gmail.com>
2022-11-15 19:39:10 +0100

    Create a placeholder MovieComparizon container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
+import styles from './App.module.css';
 import {MovieGrid} from "./components/MovieGrid";
+import {MovieComparison} from "./components/MovieComparison";
 
 function App() {
 	return (
-		<div>
+		<div className={styles.appContainer}>
+			<h1>Are These Similar?</h1>
+			<MovieComparison />
 			<MovieGrid />
 		</div>
 	);
---
+++ src/components/MovieComparison.tsx
@@ -0,0 +1,5 @@
+import styles from "./MovieComparison.module.css";
+
+export const MovieComparison = () => (
+    <div className={styles.container}>Compare</div>
+)
