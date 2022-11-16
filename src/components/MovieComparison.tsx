bb2ffc099e0afc4470c981230dfccd1288606863
Zahra memar <zahra.memar72@gmail.com>
2022-11-16 20:51:22 +0100

    Implement MovieComparison component

--- src/components/MovieComparison.tsx
+++ src/components/MovieComparison.tsx
@@ -1,5 +1,38 @@
 import styles from "./MovieComparison.module.css";
+import {Movie} from "../hooks/useGetMovies";
+import {Fragment} from "react";
 
-export const MovieComparison = () => (
-    <div className={styles.container}>Compare</div>
+type Props = {
+    left: Movie;
+    right: Movie;
+}
+
+const MovieBox = ({movie}: {movie: Movie}) => (
+    <div className={styles.movieDiv}>
+        <img className={styles.movieImg} src={movie.imageSrc} alt={movie.title} />
+    </div>
+);
+
+const ComparisonInfo = () => {
+    const similarities = [<>Movie Rating: 8/10</>];
+
+    return (
+        <div className={styles.comparisonInfo}>
+            <h3>YES</h3>
+            <button>clear selection</button>
+            <div>
+                {similarities.map((similarity, idx) => (
+                    <Fragment key={idx}>{similarity}</Fragment>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export const MovieComparison = ({left, right}: Props) => (
+    <div className={styles.container}>
+        <MovieBox movie={left} />
+        <ComparisonInfo />
+        <MovieBox movie={right} />
+    </div>
 )
