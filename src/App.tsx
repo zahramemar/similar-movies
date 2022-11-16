a5c52b12a0321b3d28dfd7b0d4c2ca1f328bf600
Zahra memar <zahra.memar72@gmail.com>
2022-11-16 20:50:28 +0100

    Add a header and max-width to App and move MovieGrid logic to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import './App.css';
 import styles from './App.module.css';
 import {MovieGrid} from "./components/MovieGrid";
 import {MovieComparison} from "./components/MovieComparison";
+import {useGetMovies} from "./hooks/useGetMovies";
 
 function App() {
+	const {data: movies, isError, isLoading} = useGetMovies();
+
+	if (isLoading) return <>Loading...</>;
+
+	if (isError) return <>Error</>;
+
 	return (
 		<div className={styles.appContainer}>
-			<h1>Are These Similar?</h1>
-			<MovieComparison />
-			<MovieGrid />
+			<h1 className={styles.header}>Are These Similar?</h1>
+			<MovieComparison  left={movies![0]} right={movies![1]} />
+			<MovieGrid movies={movies!} />
 		</div>
 	);
 }
