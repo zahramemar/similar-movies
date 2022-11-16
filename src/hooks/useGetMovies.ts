40947cc3064429aa03f6295de2269110865be32d
Zahra memar <zahra.memar72@gmail.com>
2022-11-16 22:20:48 +0100

    Add id prop to movies for selection handling

--- src/hooks/useGetMovies.ts
+++ src/hooks/useGetMovies.ts
@@ -2,6 +2,7 @@ import {getPage} from "../api/content";
 import {useQuery} from "@tanstack/react-query";
 
 export type Movie = {
+    id: string;
     imageSrc: string;
     title: string;
     duration: string;
@@ -13,10 +14,11 @@ type ArrElement<ArrType> = ArrType extends readonly (infer ElementType)[]
     : never;
 
 const processData = (data: Awaited<ReturnType<typeof getPage>>): Movie[] => {
-    const {templates, products} = data.blocks[0];
-    const imageTemplate = "https://api.lorem.space/image/movie?w=240&h=320&title={title}"; // This was ugly so I replaced it: templates.find(t => t.type === 'image')!.href;
+    const {/*templates, */ products} = data.blocks[0];
+    const imageTemplate = "https://api.lorem.space/image/movie?w=240&h=320&title={title}"; // This was ugly, so I replaced it: templates.find(t => t.type === 'image')!.href;
 
     const fromProduct = (prod: ArrElement<typeof products>): Movie => ({
+        id: prod.guid,
         imageSrc: imageTemplate.replace('{title}', encodeURIComponent(prod.title)),
         year: prod.production.year,
         title: prod.title,
